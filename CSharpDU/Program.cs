c73df8c4e42aa14a893a5255b8c9c60b9496e7c7
Michael Ducharm <ducharmdev@gmail.com>
2021-07-23 16:36:07 -0400

    calculate area of shape list

--- CSharpDU/Program.cs
+++ CSharpDU/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpDU;
 
-namespace CSharpDU
+List<IShape> shapes = new()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    new Circle(5),
+    new Square(5),
+    new Rectangle(5, 5),
+    new Triangle(5, 5),
+    new Circle(10),
+    new Square(10),
+    new Rectangle(10, 5),
+    new Triangle(5, 10),
+};
+
+List<double> areas = shapes.Select(Shape.CalculateArea).ToList();
+
+foreach (var area in areas)
+{
+    Console.WriteLine(area);
 }
