66ca53e19c4372a49a011b2ec5003d1f3a500a9a
Michael Ducharm <ducharmdev@gmail.com>
2021-07-23 16:37:12 -0400

    calc areas using shape module

--- FSharpDU/Program.fs
+++ FSharpDU/Program.fs
@@ -1,13 +1,22 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+open Shape
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+
+
+    let shapes =
+        [ Circle(5.)
+          Square(5.)
+          Rectangle(5., 5.)
+          Triangle(5., 5.)
+          Circle(10.)
+          Square(10.)
+          Rectangle(10., 5.)
+          Triangle(5., 10.) ]
+
+    shapes
+    |> List.map calculateArea
+    |> List.iter (printfn "%f")
+
     0 // return an integer exit code
